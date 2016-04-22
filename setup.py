d4a00a6f7d99dadd7270295b5dc9edd74d9e6613
Ken Sugino <ken.sugino@gmail.com>
2016-04-22 17:42:03 -0400

    changed setup
    Former-commit-id: c4572826d2badf4e00d7b104f2ecefbcd33375e2 [formerly 6c415d82c846129150a732406b636294549f625b]
    Former-commit-id: 6265bbbd19d2df420c29a981b2890483b3812236

--- setup.py
+++ setup.py
@@ -113,8 +113,8 @@ setup(
         #'dev': ['check-manifest'],
         #'test': ['coverage'],
     },
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
+    #setup_requires=['pytest-runner'],
+    #tests_require=['pytest'],
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
