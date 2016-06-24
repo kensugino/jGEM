d8c9a8a3e3f37532d901d3d7d51bd6c4bf10e5cc
Ken Sugino <ken.sugino@gmail.com>
2016-06-24 02:37:09 -0400

    logcov

--- jgem/assembler3.py
+++ jgem/assembler3.py
@@ -2085,41 +2085,41 @@ class LocalAssembler(object):
         ax.set_frame_on(False)
         return ax
 
-    def drawspan2(self,st,ed,strand,df2, win=500, figsize=(15,6),  delta=500, df2cov='sc2', maxdisp=None):
+    def drawspan2(self,st,ed,strand,df2, win=500, figsize=(15,6),  delta=500, df2cov='sc2', maxdisp=None,logcov=True):
         fig, axr = P.subplots(2,1,figsize=figsize,sharex=True)
         P.subplots_adjust(hspace=0)
-        self.draw_covs(st,ed,strand, ax=axr[0], win=win)        
+        self.draw_covs(st,ed,strand, ax=axr[0], win=win, logcov=logcov)        
         self.draw_path(df2, st,ed,strand, ax=axr[1], covfld=df2cov, win=win, delta=delta, maxdisp=maxdisp)
 
-    def drawspan2pn(self,st,ed,df2, win=500, figsize=(15,6),  delta=500, df2cov='sc2', maxdisp=None):
+    def drawspan2pn(self,st,ed,df2, win=500, figsize=(15,6),  delta=500, df2cov='sc2', maxdisp=None,logcov=True):
         fig, axr = P.subplots(4,1,figsize=figsize,sharex=True)
         P.subplots_adjust(hspace=0)
-        self.draw_covs(st,ed,'+', ax=axr[0], win=win)
+        self.draw_covs(st,ed,'+', ax=axr[0], win=win, logcov=logcov)
         self.draw_path(df2, st,ed,'+', ax=axr[1], covfld=df2cov, win=win, delta=delta, maxdisp=maxdisp)
-        self.draw_covs(st,ed,'-', ax=axr[2], win=win)
+        self.draw_covs(st,ed,'-', ax=axr[2], win=win, logcov=logcov)
         self.draw_path(df2, st,ed,'-', ax=axr[3], covfld=df2cov, win=win, delta=delta, maxdisp=maxdisp)
 
     def drawspan3(self,st,ed,strand,df2,df3,win=500, figsize=(15,9), delta=500, 
-        df2cov='sc2', df3cov='tcov', maxdisp=None):
+        df2cov='sc2', df3cov='tcov', maxdisp=None, logcov=True):
         fig, axr = P.subplots(3,1,figsize=figsize,sharex=True)
         P.subplots_adjust(hspace=0)
-        self.draw_covs(st,ed,strand, ax=axr[0], win=win)
+        self.draw_covs(st,ed,strand, ax=axr[0], win=win, logcov=logcov)
         self.draw_path(df2, st,ed,strand, ax=axr[1], covfld=df2cov, win=win, delta=delta, maxdisp=maxdisp)
         self.draw_path(df3, st,ed,strand, ax=axr[2], win=win, delta=delta, covfld=df3cov, maxdisp=maxdisp)
 
     def drawspan3pn(la,st,ed,win=10000, figsize=(15,6), df2=None, df3=None, delta=500,
-        df2cov='sc2', df3cov='tcov', maxdisp=None):
+        df2cov='sc2', df3cov='tcov', maxdisp=None, logcov=True):
         o = la.st
         fig, axr = P.subplots(6,1,figsize=figsize,sharex=True)
         P.subplots_adjust(hspace=0)
         strand = '+'
-        la.draw_covs(st,ed,strand, ax=axr[0], win=win)
+        la.draw_covs(st,ed,strand, ax=axr[0], win=win, logcov=logcov)
         if df2 is not None:
             la.draw_path(df2, st,ed,strand, ax=axr[1], covfld=df2cov, win=win, delta=delta, maxdisp=maxdisp)
         if df3 is not None:
             la.draw_path(df3, st,ed,strand, ax=axr[2], win=win, delta=delta, covfld=df3cov, maxdisp=maxdisp)
         strand = '-'
-        la.draw_covs(st,ed,strand, ax=axr[3], win=win)
+        la.draw_covs(st,ed,strand, ax=axr[3], win=win, logcov=logcov)
         if df2 is not None:
             la.draw_path(df2, st,ed,strand, ax=axr[4], covfld=df2cov, win=win, delta=delta, maxdisp=maxdisp)
         if df3 is not None:
