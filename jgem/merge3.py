7d0b154f5fada75cb2a45fbc4f7f847485a5b3ec
Ken Sugino <ken.sugino@gmail.com>
2016-06-23 22:14:15 -0400

    geometric mean of tcov0a,tcov0b,tcov0c

--- jgem/merge3.py
+++ jgem/merge3.py
@@ -783,7 +783,8 @@ class LocalEstimator(A3.LocalAssembler):
             pg['tcov0b'] = (cov1s(s)+cov1e(e))/2.
             pg['tcov0c'] = (cov2s(s)-cov2e(e))/2.
 
-        pg['tcov0'] = pg[['tcov0a','tcov0b','tcov0c']].mean(axis=1)
+        # pg['tcov0'] = pg[['tcov0a','tcov0b','tcov0c']].mean(axis=1)
+        pg['tcov0'] = N.power(pg['tcov0a']*pg['tcov0b']*pg['tcov0c'], 1/3.) # geometric mean
         pg.loc[pg['tcov0']<0,'tcov0'] = 0 # shouldn't really happen
         keys = [tuple(x) for x in p[idx][['tst','ted']].values]
         for f in ['tcov0','tcov0a','tcov0b','tcov0c']:
